df87608d47dd572cfa0543d99bbe00a3bd711757
PraneethV-cmd <praneethv375@gmail.com>
2026-01-19 17:21:03 +0530

    feat: add AST expr classes for number, binary, vars, functions

--- frontend.cpp
+++ frontend.cpp
@@ -110,3 +110,50 @@ static int gettok() {
 	LastChar = getchar();
 	return ThisChar;
 }
+
+// ExprAST -  base class for all expression nodes
+
+class ExprAST {
+public:
+	virtual ~ExprAST() = default; // this is a destructor, called when an object is destroyed to free memory, clase files, release resources, clean up owned objects
+	// generally speaking, destructors are virutal becasue we need to delete derived objects through base class pointers, casuing memory leaks
+};
+
+// NumberExprAST - expression class for numerical literals like "1.0"
+
+class NumberExprAST : public ExprAST {
+	double Val;
+
+public:
+	NumberExprAST(double Val) : Val(Val) {}
+};
+
+class VariableExprAST : public ExprAST {
+	std::string Name;
+public:
+	VariableExprAST(std::string Name) : Name(Name) {}
+};
+
+class BinaryExprAST : public ExprAST {
+	char Op;
+	std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+	BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS) : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+// CallExprAST - expresseion class for function calls 
+
+class CallExprAST : public ExprAST {
+	std::string Callee;
+	std::vector<std::unique_ptr<ExprAST>> Args;
+public:
+	CallExprAST(const std::string &Callee, std::vector<std::unique_ptr<ExprAST>> Args) : Callee(Callee), Args(std::move(Args)) {}
+};
+
+
+
+
+
+
+
